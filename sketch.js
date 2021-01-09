c4fe39d5d38dd00c043ebc41896388722bc08d3f
OmWadalkar <73993942+OmWadalkar@users.noreply.github.com>
2021-01-09 15:58:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canvas,backgroundImg;
+var gameState = 0
+var playerCount;
+var database;
+var form,game,player;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  canvas  = createCanvas(500,500);
+ game = new Game();
+ game.getState();
+ game.start();
+  
+}
+
+function draw(){
+
+ 
+    }
+  
